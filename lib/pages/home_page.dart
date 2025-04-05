2a86c6a28f06853f5ecfa2e3abed119f4fcad91a
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2025-04-05 22:37:19 +0100

    test video service

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:orange_valley_caa/services/vide_api.dart';
+
 
 class HomePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+      getVideoFromApi().then((value)=> print(value));
     return Scaffold(
       appBar: AppBar(title: Text('Orange Valley CAA'),),
       body: Container(),
