82c46190241e0599f2cabbedee7fbae6e3cbe0d0
Herman-Motcheyo <hermanmotcheyo@gmail.com>
2025-04-05 23:09:28 +0100

    add widgets folder and file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:orange_valley_caa/services/vide_api.dart';
+import 'package:orange_valley_caa/utils/constants.dart';
+import 'package:orange_valley_caa/utils/helper.dart';
+import 'package:orange_valley_caa/widgets/video_grid.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -8,8 +11,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
       getVideoFromApi().then((value)=> print(value));
     return Scaffold(
-      appBar: AppBar(title: Text('Orange Valley CAA'),),
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Orange Valley CAA'),
+        backgroundColor: Colors.black,
+        titleTextStyle: TextStyle(color: Colors.white)),
+      body: Container(
+        color: backgroundColor,
+        child: VideosGrid(videos: getFakeVideos()),
+      ),
     );
   }
 }
